d265447dfbcc65bab63b122a455f1b224f1ac45f
ufuoma iboyi <ufuomaiboyi23@yahoo.com>
2023-11-10 12:31:48 -0500

    all task done successfully

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,9 +34,19 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(books)
+    }, 6000);
+})
 public_users.get('/', function (req, res) {
     //Write your code here
-    return res.status(300).send(JSON.stringify(books, null, 10));
+    promise1.then((success) => {
+        return res.status(300).send(JSON.stringify(success, null, 10));
+    }).catch((error) => {
+        next(error);
+    });
+    // return res.status(300).send(JSON.stringify(books, null, 10));
 });
 
 // Get book details based on ISBN
@@ -44,7 +54,12 @@ public_users.get('/isbn/:isbn', function (req, res) {
     //Write your code here
     const ISBN = parseInt(req.params.isbn);
     if (ISBN >= 1 && ISBN <= 10) {
-        return res.status(300).send(JSON.stringify(books[ISBN], null, 10))
+        promise1.then((success) => {
+            return res.status(300).send(JSON.stringify(success[ISBN], null, 10));
+        }).catch((error) => {
+            next(error);
+        });
+        // return res.status(300).send(JSON.stringify(books[ISBN], null, 10))
     }
     else {
         return res.status(300).send('This book does not exist')
@@ -52,65 +67,90 @@ public_users.get('/isbn/:isbn', function (req, res) {
     //   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+function fetchAuthor(author) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (author) {
+                const isbn = Object.keys(books).length;
+
+                const authorlist = [];
+                const pointer = [];
+
+                for (let index = 1; index < isbn; index++) {
+                    const value = books[index]['author'];
+                    authorlist.push(value);
+                }
+
+
+                const real = authorlist.filter((names, index) => {
+                    if (names === author) {
+                        pointer.push(books[index + 1])
+                        return names
+                    }
+                })
+                resolve(pointer);
+            }
+            else {
+                reject('invalid request URL')
+            }
+        })
+    }, 6000)
+};
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     //Write your code here
     const author = req.params.author;
-    if (author) {
-        const isbn = Object.keys(books).length;
 
-        const authorlist = [];
-        const pointer = [];
-
-        for (let index = 1; index < isbn; index++) {
-            const value = books[index]['author'];
-            authorlist.push(value);
-        }
-
-
-        const real = authorlist.filter((names, index) => {
-            if (names === author) {
-                pointer.push(books[index + 1])
-                return names
-            }
-        })
-        if (pointer.length > 0) {
+    fetchAuthor(author).then((success) => {
+        if (success.length > 0) {
             res.status(300).send(JSON.stringify(pointer, null, 4))
         } else {
             res.status(300).send('this author does not exist');
         }
-    } else {
+    }).catch((error) => {
         res.status(300).send('invalid request URL')
-    }
+    });
+
     //   return res.status(300).json({message: "Yet to be implemented"});
 });
+
+
+function fetchTitle(title){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const list = Object.keys(books).length;
+            let bookTitle=[];
+            let pointer1=[];
+            if (title) {
+                for (let index = 1; index < list; index++) {
+                    const value = books[index]['title'];
+                    bookTitle.push(value);
+                }
+                const real = bookTitle.filter((names, index) => {
+                    if (names === title) {
+                        pointer1.push(books[index + 1])
+                        return names
+                    }
+                });
+             resolve(pointer1)
+            }
+        },9000)
+    })
+}
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     //Write your code here
     const title = req.params.title;
-    let bookTitle = [];
-    const pointer1 = [];
-    const list = Object.keys(books).length;
-    if (title) {
-        for (let index = 1; index < list; index++) {
-            const value = books[index]['title'];
-            bookTitle.push(value);
-        }
-        const real = bookTitle.filter((names, index) => {
-            if (names === title) {
-                pointer1.push(books[index + 1])
-                return names
-            }
-        });
-        if (pointer1.length > 0) {
-            res.status(300).send(JSON.stringify(pointer1, null, 4))
+     fetchTitle(title).then((success)=>{
+        if (success.length > 0) {
+            res.status(300).send(JSON.stringify(success, null, 4))
         } else {
             res.status(300).send('this book does not exist');
         }
-    } else {
+     }).catch((error)=>{
         res.status(300).send('invalid request URL');
-    }
+     })
     //   return res.status(300).json({message: "Yet to be implemented"});
 });
 
